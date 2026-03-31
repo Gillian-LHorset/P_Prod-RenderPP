1b5674d3fd08e5941b5f4befb02ab4b8f0990d1a
Gillian-LHorset <pq60soi@eduvaud.ch>
2026-03-31 10:01:55 +0200

    feat(CMakeLists): Ajout de la dépendence GLFW pour créer une fênetre
    [170][DONE]

--- RenderPP/main.cpp
+++ RenderPP/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // crash the app
+    if (!glfwInit()) {
+        std::cerr << "Échec de l'initialisation de GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 800, "Render++", NULL, NULL);
+    if (!window) {
+        std::cerr << "Échec de la création de la fenêtre" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // main while of the window
+    while (!glfwWindowShouldClose(window)) {
+
+        glfwSwapBuffers(window);
+        // manage key events
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
